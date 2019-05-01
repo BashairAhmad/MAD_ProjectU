27d5d4f5fe46441b6adfabba9f504b1dbf092ef2
BashairAhmad <bashair.ahmad861@ucp.edu.pk>
2019-05-01 17:47:33 +0500

    adding

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mad_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,11 +81,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.Reservations) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+            Intent i = new Intent (this , reservations.class);
+            startActivity(i);
+        } else if (id == R.id.Menu) {
+
+        } else if (id == R.id.Home_Delivery) {
 
         } else if (id == R.id.nav_manage) {
 
